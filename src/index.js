dcfe6832eb39f3528d13d1a68173440be8767978
sameep-s <sameep.sharma98@gmail.com>
2022-04-01 18:04:23 +0530

    signupcss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
+
 import { Signup } from "./components/index";
+
 import { makeServer } from "./server";
 
 // Call make Server
