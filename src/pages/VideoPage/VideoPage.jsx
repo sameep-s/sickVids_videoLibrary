e5417721372cfcc7ec617f74743f99e2276c64bf
sameep-s <sameep.sharma98@gmail.com>
2022-04-06 06:27:46 +0530

    suggestionVideoPage

--- src/pages/VideoPage/VideoPage.jsx
+++ src/pages/VideoPage/VideoPage.jsx
@@ -39,6 +39,27 @@ const VideoPage = () => {
 
                     <div className="container__suggestionVideos">
 
+                        <div className="container__suggestionVideo flex mb-2">
+                            <div className="container__image__suggestionVideo">
+                                <img src="https://i.ytimg.com/vi/hPJ762sAptY/maxresdefault.jpg" alt="suggestionVideoThumbnail" />
+                            </div>
+
+                            <div className="suggestionVideo__content">
+                                <div className="suggestionVideo__title flex ml-1">
+                                    THE BEST 'SUHAGRAAT' VLOG EVER | LAKSHAY CHAUDHARY
+                                </div>
+
+                                <div className="suggestionVideo__info pl-1">
+                                    <div className="suggestionVideo__info__creator ">
+                                        Lakshay Choudhary
+                                    </div>
+                                    <div className="suggestionVideo__info__views">
+                                        666 views
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
